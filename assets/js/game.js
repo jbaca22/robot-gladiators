e091907aa73f08c7d15ebacc1c069c064759de2e
Jarred Baca <jarredbaca@MacBook-Pro.local>
2020-05-17 15:43:43 -0600

    added numbers to shop menu

--- assets/js/game.js
+++ assets/js/game.js
@@ -122,20 +122,17 @@ var endGame = function() {
 
 var shop = function() {
   var shopOptionPrompt = window.prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
   );
+  shopOptionPrompt = parseInt(shopOptionPrompt);
   switch (shopOptionPrompt) {
-    case "REFILL":
-      case "refill":
-        playerInfo.refillHealth();
-        break;
-      case "UPGRADE":
-      case "upgrade":
-        playerInfo.upgradeAttack();
-        break;
+    case 1:
+      playerInfo.refillHealth();
       break;
-    case "LEAVE": // new case
-    case "leave":
+    case 2:
+      playerInfo.upgradeAttack();
+      break;
+    case 3:
       window.alert("Leaving the store.");
       break;
     default:
